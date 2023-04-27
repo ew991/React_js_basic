8724289579e17f8e451fa1f382c9310d6b54ba6f
Toan <nguyenphuctoan01@gmai.com>
2023-04-27 14:46:14 +0700

    import hinh anh

--- src/views/Home/home.js
+++ src/views/Home/home.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import Hinh from "../../assets/images/images.jpeg"
 class Home extends React.Component {
     render() {
         return (
-            <div>Hello world from Home </div>
+            <>
+                <div>Hello world from Home </div>
+                <div>
+                    <img src={Hinh} />
+                </div>
+            </>
+
 
         )
     }
